4fe0fa67dc35a940ac33f27fa48385dc354b7c37
turkestanec <madiaitzhan03@gmail.com>
2025-05-27 14:39:01 +0500

    IT IZI

--- api/signin.php
+++ api/signin.php
@@ -1,18 +1,30 @@
-<?php 
+<?php
 include '../config/database.php'; 
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 if (isset($email) && isset( $password) && strlen($email) > 5 && strlen($password) > 5){
-    
+
     $query = "SELECT * FROM users WHERE email = '$email'";
     $user_query = mysqli_query($connection, $query);
+    $user_about =  mysqli_query($user_query);
 
    
-    if (mysqli_fetch_assoc($user_query ) <= 0) {
-        echo ''
+    if ($user_about <= 0) {
+        echo 'Мұндай пайдаланушы табылмады!';
     }
+
+   if($password != $user_about['password']){
+    echo 'Парольді қате енгіздіңіз!';
+   }
+
+   header('Location: http://localhost/%d0%ba%d0%bc4%20%d0%bf%d1%80%d0%b0%d0%ba%d1%82%d0%b8%d0%ba%d0%b0/20%2005%2025/');
+   session_start();
+   $_SESSION['id']=$user_about['id'];
+   $_SESSION['username']=$user_about['username'];
+}else{
+    echo 'Мәліметтерді толық енгізіңіз';
 }
 
 ?>
